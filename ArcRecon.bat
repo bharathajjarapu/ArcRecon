d337f9dd9222fdc1c3fe8f4dd1eb48acf74dd791
Bharath <106143429+bharathajjarapu@users.noreply.github.com>
2023-10-28 12:17:32 +0530

    checks

--- ArcRecon.bat
+++ ArcRecon.bat
@@ -15,118 +15,24 @@ echo ==========================
 echo     Arc Recon Toolbox
 echo ==========================
 echo.
-echo 1. Activate Your OS
-echo 2. Install Foffice
-echo 3. Activate FOffice
-echo 4. Remove OneDrive
-echo 5. Run ChrisTitusTool
-echo 6. Activate Spicetify
-echo 7. Right Click Ownership
-echo 8. Install Brave Browser
-echo 9. Auto Game Configuration
-echo 10. Exit
+echo 1. Run ChrisTitusTool
+echo 2. Remove OneDrive
+echo 3. Activate Spicetify
+echo 4. Right Click Ownership
+echo 5. Install Brave Browser
+echo 6. Auto Game Configuration
+echo 7. Exit
 echo.
 
 set /p choice=Enter your choice : 
 
-if "%choice%"=="1" goto activate
-if "%choice%"=="2" goto foffice
-if "%choice%"=="3" goto office21
-if "%choice%"=="4" goto onedrive
-if "%choice%"=="5" goto ctt
-if "%choice%"=="6" goto freespotify
-if "%choice%"=="7" goto rcowner
-if "%choice%"=="8" goto brave
-if "%choice%"=="9" goto gamercfg
+if "%choice%"=="2" goto onedrive
+if "%choice%"=="1" goto ctt
+if "%choice%"=="3" goto freespotify
+if "%choice%"=="4" goto rcowner
+if "%choice%"=="5" goto brave
+if "%choice%"=="6" goto gamercfg
 if "%choice%"=="10" goto exiter
-
-:activate
-set /P achoice=Do you want to Activate Windows ? (Y/N): 
-if /i "%achoice%" EQU "Y" goto windowsact
-if /i "%achoice%" EQU "N" goto menu
-
-:windowsact
-    echo Select your Windows version:
-    echo 1. Home    
-    echo 2. Home Single Language
-    echo 3. Education
-    echo 4. Enterprise
-    echo 5. Professional
-
-    set /p wchoice=Enter your choice (1-5):
-
-    if "%wchoice%"=="1" (
-    set key=TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
-    goto activate
-    )
-
-    if "%wchoice%"=="2" (
-    set key=W269N-WFGWX-YVC9B-4J6C9-T83GX
-    goto activate
-    )
-
-    if "%wchoice%"=="3" (
-    set key=NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
-    goto activate
-    )
-
-    if "%wchoice%"=="4" (
-    set key=NPPR9-FWDCX-D2C8J-H872K-2YT43
-    goto activate
-    )
-
-    if "%wchoice%"=="5" (
-    set key=W269N-WFGWX-YVC9B-4J6C9-T83GX
-    goto activate
-    )
-
-    :activate
-    echo Installing license key...
-    slmgr /skms kms8.msguides.com
-
-    :retry
-    slmgr /ipk %key%
-    if errorlevel 0 (
-        goto end
-    )
-
-    echo Error- Server busy. Retrying...
-    timeout /t 5 >nul
-    goto retry
-
-    :end
-    slmgr /ato
-    echo Activation complete.
-    Pause
-    goto menu
-
-:foffice
-    set /p "Consent=Do you want to install Office 2021? (Y/N): "
-    if /i "%Consent%" EQU "y" (
-      echo Starting Office setup...
-      echo.
-    start /wait setup /configure "%ConfigFile%"
-
-    echo Office setup completed.
-    pause
-    echo Exiting...
-    goto menu
-    ) else goto menu
-
-:office21
-      set /p "ActivateConsent=Do u want to activate the Office 2021 ? (Y/N): "
-      if /i "%ActivateConsent%" EQU "y" (
-      if exist "C:\Program Files\Microsoft Office\Office16\ospp.vbs" cd /d "C:\Program Files\Microsoft Office\Office16"
-      if exist "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" cd /d "C:\Program Files (x86)\Microsoft Office\Office16"
-      for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x"
-      cscript ospp.vbs /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH
-      cscript ospp.vbs /sethst:kms.msgang.com
-      cscript ospp.vbs /act
-      pause
-      echo Exiting...
-      pause
-      goto menu
-    ) else goto menu
 
 :onedrive
     set /P onechoice=Do you want to remove Onedrive ? (Y/N): 
@@ -157,7 +63,6 @@ if /i "%achoice%" EQU "N" goto menu
         pause
         goto menu
     ) else goto menu
-
 
 :ctt
     set /P cttchoice=Do you want to use the Chris Titus tool? (Y/N): 
@@ -251,7 +156,7 @@ if /i "%achoice%" EQU "N" goto menu
         echo Failed to create the Ultimate Performance power plan.
         goto menu
     )
-    endlocal
+    goto begin
 
 :exiter
     if exist "%OfficePath%"(
